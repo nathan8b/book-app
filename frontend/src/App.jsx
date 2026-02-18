cf9c09c408e168d6cf70338be75bf8a60ab7b227
nathan8b <nb803622@ohio.edu>
2026-02-17 22:41:05 -0500

    added SearchBar component with navigation to search page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import BookList from './components/BookList'
 import BookDetail from './components/BookDetail'
+import SearchBar from './components/SearchBar'
 import './App.css'
 
 function App() {
   return (
     <div className='App'>
+      <SearchBar></SearchBar>
       <Routes>
         <Route path='/' element={<BookList/>}></Route>
         <Route path='/books/:id' element={<BookDetail/>}></Route>
---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,26 @@
+import { useState} from 'react'
+import {useNavigate } from 'react-router-dom'
+
+function SearchBar() {
+    const [search, setSearch] = useState('')
+    const navigate = useNavigate()
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        navigate(`/search?q=${encodeURIComponent(search)}`)
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type='text'
+                placeholder='Title / Author / ISBN'
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            <button type='submit'>Submit</button>
+        </form>
+    )
+
+}
+
+export default SearchBar
