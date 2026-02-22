f5f7224281d725f3531a561a017dba5b2d329bcf
nathan8b <nb803622@ohio.edu>
2026-02-21 23:32:02 -0500

    clicking search result saves to db and navs to book detail page

--- backend/books/views.py
+++ backend/books/views.py
@@ -1,11 +1,25 @@
 from rest_framework import viewsets
 from .models import Book, Review
 from .serializers import BookSerializer, ReviewSerializer
+from rest_framework.response import Response
 
 # Create your views here.
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def create (self, request, *args, **kwargs):
+        data = request.data
+        obj, created = Book.objects.get_or_create(
+            isbn = data['isbn'],
+            defaults={'title': data['title'],
+                      'author': data['author'],
+                      'description': data['description'],
+                      'pub_date': data['pub_date']}
+        )
+        return Response(BookSerializer(obj).data)
+
+
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
--- frontend/src/components/SearchResults.jsx
+++ frontend/src/components/SearchResults.jsx
@@ -1,4 +1,4 @@
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from 'react'
 import '../styles/SearchResults.css'
 
@@ -7,6 +7,7 @@ function SearchResults() {
     const [books, setBooks] = useState([])
     const [searchParams] = useSearchParams();
     const query = searchParams.get('q')
+    const navigate = useNavigate()
 
     useEffect(() => {
         fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:${query}&key=${import.meta.env.VITE_GOOGLE_BOOKS_KEY}`)
@@ -18,14 +19,33 @@ function SearchResults() {
         .catch(error => console.error('Search error:', error))
     }, [query])
 
-    if (!books) {
-        return <div>Loading...</div>
+    function handleClick (book) {
+        const bookData = {
+            title: book.volumeInfo.title,
+            author: book.volumeInfo.authors.join(", "),
+            isbn: book.volumeInfo.industryIdentifiers?.find(isbn => isbn.type === 'ISBN_13')?.identifier || null,
+            description: book.volumeInfo.description || null,
+            pub_date: book.volumeInfo.publishedDate?.length === 10 ? book.volumeInfo.publishedDate : null,
+        }
+
+        fetch('http://localhost:8000/api/books/', {
+            method: 'POST',
+            body: JSON.stringify(bookData),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            navigate(`/books/${data.id}`)
+        })
+        
     }
 
     return (
         <div className="results-container">
             {books.map(book => (
-                <div className="results-book-card" key={book.id}>
+                <div className="results-book-card" key={book.id} onClick={() => handleClick(book)}>
                     <h2>{book.volumeInfo.title}</h2>
                     {book.volumeInfo.subtitle && <p>{book.volumeInfo.subtitle}</p>}
                     <p>by <strong>{book.volumeInfo.authors.join(', ')}</strong></p>
