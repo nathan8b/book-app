ebdfa95961e20c8554974863218132fe49e0b73d
nathan8b <nb803622@ohio.edu>
2026-02-24 17:25:16 -0500

    create users app with Profile and UserBooks models

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'books',
+    'users',
     'rest_framework',
 ]
 
---
+++ backend/users/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+from books.models import Book
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='profile'
+    )
+    bio = models.TextField(blank=True, null=True)
+    pfp = models.ImageField(upload_to='pfps', blank=True, null=True)
+
+class UserBook(models.Model):
+    STATUS_CHOICES = {
+        'READ': 'Read',
+        'WANT': 'Want to read',
+        'CURR': 'Currently reading',
+    }
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='book_users')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_books')
+    status = models.CharField(max_length=4, choices=STATUS_CHOICES, default='WANT')
+    rating = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(1), MaxValueValidator(10)])
+    review = models.TextField(blank=True, null=True)
